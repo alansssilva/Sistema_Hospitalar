f61dcdc207c11f17811b0f58d32873e337ea4297
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-09-02 20:16:47 -0300

    Delete WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/repository /repository  directory

--- WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/repository /repository /ComumRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.iff.ccc.bsi.SistemaHospitalar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.iff.ccc.bsi.SistemaHospitalar.entities.Comum;
-
-public interface ComumRepository extends JpaRepository<Comum, Integer> {
-}

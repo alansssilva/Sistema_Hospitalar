1f9163aebf94c7a92171292a85ab957083eb9b7f
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-09-02 20:04:59 -0300

    Create HomeController.java

---
+++ WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/controller/view/HomeController.java
@@ -0,0 +1,15 @@
+package br.edu.iff.ccc.bsi.SistemaHospitalar.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home"; // Nome do arquivo HTML que será exibido
+    }
+}

e6fbdb37d9278ee0f6b0747508f652390b4ca4a9
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-09-09 15:48:47 -0300

    Update ConsultaRepository.java

--- WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/repository /ConsultaRepository.java
+++ WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/repository /ConsultaRepository.java
@@ -1,7 +1,13 @@
 package br.edu.iff.ccc.bsi.SistemaHospitalar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import br.edu.iff.ccc.bsi.SistemaHospitalar.entities.Consulta;
+import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
+
+    //QueryMethod buscando consultas por nome do médico e descrição:
+    @Query("SELECT c FROM Consulta c WHERE c.medico.nome = :nomeMedico AND c.descricao LIKE %:descricao%")
+    List<Consulta> findByMedicoNomeAndDescricao(String nomeMedico, String descricao);
 }

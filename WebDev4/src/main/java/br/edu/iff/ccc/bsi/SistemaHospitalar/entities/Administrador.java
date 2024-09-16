50c6147d4da5d53e34c87e44a2ce4d73b65cbbb9
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-09-16 14:52:47 -0300

    Update Administrador.java

--- WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/entities/Administrador.java
+++ WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/entities/Administrador.java
@@ -2,21 +2,23 @@ package br.edu.iff.ccc.bsi.SistemaHospitalar.entities;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Administrador extends Usuario implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private LocalDateTime dataAdmissao;
+    @NotNull
+    private LocalDateTime dataAdmissao;
 
-	public LocalDateTime getDataAdmissao() {
-		return dataAdmissao;
-	}
+    
+    public LocalDateTime getDataAdmissao() {
+        return dataAdmissao;
+    }
 
-	public void setDataAdmissao(LocalDateTime dataAdmissao) {
-		this.dataAdmissao = dataAdmissao;
-	}
+    public void setDataAdmissao(LocalDateTime dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
 }

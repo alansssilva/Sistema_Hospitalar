2e97e436d26bb08d9c2085c064be7c8fac6884ec
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-09-30 21:44:05 -0300

    Update MedicoController.java

--- WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/controller/apirest/MedicoController.java
+++ WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/controller/apirest/MedicoController.java
@@ -1,94 +1,41 @@
 package com.tiagoamp.usuarioapi.controller;
 
-import com.tiagoamp.usuarioapi.dto.*;
+import com.tiagoamp.usuarioapi.dto.UsuarioResponse;
+import com.tiagoamp.usuarioapi.dto.RootEntryPointResponse;
 import com.tiagoamp.usuarioapi.service.UsuarioService;
-import com.tiagoamp.usuarioapi.util.UsuarioMapper;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/usuarios")
-@RequiredArgsConstructor
 public class UsuarioController {
 
-    private final UsuarioService service;
-    private final UsuarioMapper usuarioMapper;
+    private final UsuarioService usuarioService;
 
-    @Operation(summary = "Buscar usuários", description = "Obter usuários registrados")
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @Operation(summary = "Buscar todos os usuários", description = "Retorna uma lista de usuários cadastrados.")
     @GetMapping
-    public ResponseEntity<List<UsuarioResponse>> getUsuarios(
-            @RequestParam(value = "size", required = false, defaultValue = "3") Integer size,
-            @RequestParam(value = "page", required = false, defaultValue = "0") Integer pageNumber,
-            @RequestParam(value = "sort", required = false, defaultValue = "nome") String sortField,
-            @RequestParam(value = "direction", required = false, defaultValue = "ASC") String sortDirection) {
-        var usuarios = service.findUsuarios(size, pageNumber, sortField, sortDirection);
-        var usuariosResp = usuarios.stream()
-                .map(usuarioMapper::toResponse)
-                .map(u -> u.add(linkTo(methodOn(this.getClass()).getUsuario(u.getId())).withSelfRel()))
-                .toList();
-        return ResponseEntity.ok(usuariosResp);
+    public ResponseEntity<RootEntryPointResponse> getUsuarios() {
+        RootEntryPointResponse resp = new RootEntryPointResponse()
+                .add(linkTo(methodOn(UsuarioController.class).getUsuarios())
+                        .withRel("usuarios"));
+        return ResponseEntity.ok(resp);
     }
 
-    @Operation(summary = "Buscar usuário por id", description = "Buscar usuário por id",
-            responses = {@ApiResponse(responseCode = "404", description = "Usuário não encontrado",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})})
+    @Operation(summary = "Buscar usuário por ID", description = "Retorna um usuário específico pelo ID.")
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioResponse> getUsuario(@PathVariable("id") Integer id) {
-        var usuario = service.findUsuarioById(id);
-        var usuarioResp = usuarioMapper.toResponse(usuario)
-                .add(linkTo(methodOn(this.getClass()).getUsuarios(null, null, null, null)).withRel("usuarios"));
-        return ResponseEntity.ok(usuarioResp);
-    }
-
-    @Operation(summary = "Registrar novo usuário", description = "Registrar novo usuário",
-            responses = {@ApiResponse(responseCode = "400", description = "Dados de requisição inválidos",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})})
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED) 
-    public ResponseEntity<UsuarioResponse> createUsuario(@RequestBody @Valid UsuarioRequest request) {
-        var usuario = usuarioMapper.toModel(request);
-        usuario = service.createUsuario(usuario);
-        var usuarioResp = usuarioMapper.toResponse(usuario);
-        return ResponseEntity.created(URI.create("/usuarios/" + usuario.getId())).body(usuarioResp);
-    }
-
-    @Operation(summary = "Atualizar informações do usuário", description = "Atualizar informações do usuário",
-            responses = {
-                    @ApiResponse(responseCode = "400", description = "Dados de requisição inválidos",
-                            content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-                    @ApiResponse(responseCode = "404", description = "Usuário não encontrado",
-                            content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
-            })
-    @PutMapping("{id}")
-    public ResponseEntity<UsuarioResponse> updateUsuario(@PathVariable("id") Integer id, @RequestBody @Valid UsuarioRequest request) {
-        var usuario = usuarioMapper.toModel(request);
-        usuario.setId(id);
-        usuario = service.updateUsuario(usuario);
-        var usuarioResp = usuarioMapper.toResponse(usuario);
-        return ResponseEntity.ok(usuarioResp);
-    }
-
-    @Operation(
-            summary = "Deletar usuário por id", description = "Deletar usuário por id",
-            responses = {@ApiResponse(responseCode = "404", description = "Usuário não encontrado",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})})
-    @DeleteMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<Void> deleteUsuario(@PathVariable("id") Integer id) {
-        service.deleteUsuario(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<UsuarioResponse> getUsuario(@PathVariable("id") Long id) {
+        UsuarioResponse usuarioResponse = usuarioService.buscarUsuarioPorId(id);
+        return ResponseEntity.ok(usuarioResponse);
     }
 }

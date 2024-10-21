36ee2fe4811ee6b16a40fbad3531639b639933ef
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-10-21 19:02:55 -0300

    Update MedicoRepository.java

--- WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/repository /MedicoRepository.java
+++ WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/repository /MedicoRepository.java
@@ -1,7 +1,12 @@
 package br.edu.iff.ccc.bsi.SistemaHospitalar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import br.edu.iff.ccc.bsi.SistemaHospitalar.entities.Medico;
 
 public interface MedicoRepository extends JpaRepository<Medico, Integer> {
+
+    // Método para buscar médico pelo email e senha
+    @Query("SELECT m FROM Medico m WHERE m.email = :email AND m.senha = :senha")
+    Medico findByEmailAndSenha(String email, String senha);
 }

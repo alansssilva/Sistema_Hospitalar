93253c78cafe7380cd121b32d5814ea626bc908f
alansssilva <129136467+alansssilva@users.noreply.github.com>
2024-10-21 18:58:26 -0300

    Update AdministradorService.java

--- WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/service/AdministradorService.java
+++ WebDev4/src/main/java/br/edu/iff/ccc/bsi/SistemaHospitalar/service/AdministradorService.java
@@ -1,12 +1,11 @@
 package br.edu.iff.ccc.bsi.SistemaHospitalar.service;
 
-import java.util.Optional;
-
+import br.edu.iff.ccc.bsi.SistemaHospitalar.entities.Administrador;
+import br.edu.iff.ccc.bsi.SistemaHospitalar.repository.AdministradorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.edu.iff.ccc.bsi.SistemaHospitalar.entities.Administrador;
-import br.edu.iff.ccc.bsi.SistemaHospitalar.repository.AdministradorRepository;
+import java.util.Optional;
 
 @Service
 public class AdministradorService {
@@ -18,7 +17,7 @@ public class AdministradorService {
         try {
             return repo.findById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Administrador não encontrado");
+            throw new RuntimeException("Administrador não encontrado: " + e.getMessage());
         }
     }
 
@@ -26,7 +25,7 @@ public class AdministradorService {
         try {
             return repo.save(administrador);
         } catch (Exception e) {
-            throw new RuntimeException("Erro ao salvar o administrador");
+            throw new RuntimeException("Erro ao salvar o administrador: " + e.getMessage());
         }
     }
 
@@ -34,7 +33,15 @@ public class AdministradorService {
         try {
             repo.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Erro ao deletar o administrador");
+            throw new RuntimeException("Erro ao deletar o administrador: " + e.getMessage());
+        }
+    }
+
+    public Administrador findByEmailAndSenha(String email, String senha) {
+        try {
+            return repo.findByEmailAndSenha(email, senha);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar administrador pelo email e senha: " + e.getMessage());
         }
     }
 }
